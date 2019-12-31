8e40b219c8d37c1717dc857ecdefbc78637bba64
Higor Lapa <higor@lotadata.com>
2019-12-31 13:21:41 -0300

    adds more constants to sp_constants

--- lib/utils/sp_constants.dart
+++ lib/utils/sp_constants.dart
@@ -3,4 +3,5 @@ class SPConstants {
   static const String USER_PASSWORD_KEY = "user_password"; // String
   static const String USER_PIN_KEY = "user_pin"; // int
 
+  static const String IS_DARK_THEME_KEY = "is_dark_key";
 }
