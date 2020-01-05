2833a706a9fbe50fee04cf4d1c410cff84e9c901
Higor Lapa <higor@lotadata.com>
2020-01-05 14:47:13 -0300

    creates the transaction_widget file and model

---
+++ lib/models/transaction_model.dart
@@ -0,0 +1,28 @@
+import 'package:strongpay/models/coin_model.dart';
+
+class TransactionModel {
+
+  CoinModel coin;
+  int timestamp;
+  double amount;
+  TransactionType transactionType;
+
+  TransactionModel({
+    this.coin,
+    this.timestamp,
+    this.amount,
+    this.transactionType
+  }) : assert(
+    coin != null,
+    timestamp != null
+  );
+
+}
+
+
+enum TransactionType {
+
+  SEND,
+  RECEIVE
+
+}
---
+++ lib/ui/index/screens/home/widgets/transaction_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TransactionWidget extends StatefulWidget {
+  @override
+  _TransactionWidgetState createState() => _TransactionWidgetState();
+}
+
+class _TransactionWidgetState extends State<TransactionWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+
+    );
+  }
+}
