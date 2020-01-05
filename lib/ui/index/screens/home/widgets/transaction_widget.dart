4a99bfe7ed31336c08200ca76e1232b93fa91a44
Higor Lapa <higor@lotadata.com>
2020-01-05 16:11:41 -0300

    adds color to transaction_widget

--- lib/ui/index/screens/home/widgets/transaction_widget.dart
+++ lib/ui/index/screens/home/widgets/transaction_widget.dart
@@ -23,16 +23,32 @@ class _TransactionWidgetState extends State<TransactionWidget> {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      padding: EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+        border: Border.all(),
+        color: Colors.grey,
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+
+      ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          transaction.coin.image,
+          Container(
+            width: 20,
+            height: 20,
+            child: transaction.coin.image,
+          ),
           ///ToDo: Change it later
           Container(
             margin: EdgeInsets.symmetric(horizontal: 10),
-            child: Text(transaction.timestamp.toString()),
+            child: Text(transaction.amount.toString(),
+            style: TextStyle(
+              color: transaction.transactionType == TransactionType.SEND ?
+                  Colors.red : Colors.green
+            ),),
           ),
-          Text(transaction.amount.toString())
+          Text(transaction.timestamp.toString()),
         ],
       ),
     );
